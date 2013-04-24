f7bd8384f031ac8f067bf1747e3b1d6db62e1ae7
Orta <orta.therox@gmail.com>
2013-04-24 12:10:13 +0200

    Remove duplication of name in header

--- BPXLUUIDHandler.h
+++ BPXLUUIDHandler.h
@@ -4,9 +4,6 @@
 //
 //  Created by Doug Russell on 2/29/12.
 //  Copyright (c) 2012 Black Pixel. All rights reserved.
-//
-//  Created by Doug Russell on 2/29/12.
-//  Copyright (c) 2012 Black Pixel. All rights reserved.
 //  
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
